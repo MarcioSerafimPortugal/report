64772e3114d01ca2d9c44df4a3736802b325edd3
Marcio Serafim de Almeida <marcioserafimportugal@gmail.com>
2021-02-05 16:00:55 +0000

    first publication

---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#Create your models here.
+class Department(models.TextChoices):
+        department_01 = '01', 'Department 01: Novos Biopesticidas'
+        department_02 = '02', 'Department 02: Proteção de Culturas Específicas'
+        department_03 = '03', 'Department 03: Gestão de Dados e Análise de Risco'
+        department_04 = '04', 'Department 04: Monitorização e Diagnóstico'
+        department_05 = '05', 'Department 05: Novas Formulações e Matrizes para a Aplicação de Biopesticidas'
+        department_06 = '06', 'Department 06: Gestão'
+
+
+class Report(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Name:')
+    depto = models.CharField(
+        max_length=100, verbose_name='Department:',
+        #max_length=2,
+        choices= Department.choices,
+        default= Department.department_01
+    )
+    date = models.DateField(verbose_name='Week Start:')
+    description = models.TextField(blank=False, null=False, verbose_name= 'Description:')
+    
+#   class Meta:
+#       db_table = 'Report'
+
+    def __str__(self):
+        return self.user
