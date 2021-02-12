017c5a0ecb97733e16d766c1dd2604480d415456
Ubuntu <ubuntu@ip-172-31-1-247.us-east-2.compute.internal>
2021-02-12 18:02:18 +0000

    put form for department 4

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import Report
+from core.models import Report, Department4
 
 
 # Register your models here.
@@ -7,4 +7,11 @@ class ReportAdmin(admin.ModelAdmin):
     list_display = ('name', 'depto', 'date', 'description')
     list_filter = ('name', 'depto', 'date',)
 
+
+class Department4Admin(admin.ModelAdmin):
+    list_display = ('name', 'date')
+    list_filter = ('name', 'date',)
+
+
 admin.site.register(Report, ReportAdmin)
+admin.site.register(Department4, Department4Admin)
--- core/models.py
+++ core/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 #Create your models here.
 class Department(models.TextChoices):
-        department_01 = '01', 'Department 01: Novos Biopesticidas'
-        department_02 = '02', 'Department 02: Proteção de Culturas Específicas'
-        department_03 = '03', 'Department 03: Gestão de Dados e Análise de Risco'
-        department_04 = '04', 'Department 04: Monitorização e Diagnóstico'
-        department_05 = '05', 'Department 05: Novas Formulações e Matrizes para a Aplicação de Biopesticidas'
-        department_06 = '06', 'Department 06: Gestão'
+        department_01 = 'Department 01: Novos Biopesticidas'
+        department_02 = 'Department 02: Proteção de Culturas Específicas'
+        department_03 = 'Department 03: Gestão de Dados e Análise de Risco'
+        department_04 = 'Department 04: Monitorização e Diagnóstico'
+        department_05 = 'Department 05: Novas Formulações e Matrizes para a Aplicação de Biopesticida'
+        department_06 = 'Department 06: Gestão'
+
+
 
 class Report(models.Model):
     name = models.CharField(max_length=200)
@@ -20,4 +22,17 @@ class Report(models.Model):
     description = models.TextField(blank=False, null=False, verbose_name= 'Description:')
 
     def __str__(self):
-        return self.user
+        return self.name
+
+class Department4(models.Model):
+    name = models.CharField(max_length=200, verbose_name= 'Name:')
+    date = models.DateField(verbose_name='Week Starts on:')
+    research_activities = models.TextField(blank=False, null=False, verbose_name= 'Research Activities:')
+    internal_meetings = models.TextField(blank=False, null=False, verbose_name= 'Internal Meetings:')
+    external_meetings = models.TextField(blank=False, null=False, verbose_name= 'External Meetings and Stakeholders Engagement:')
+    service_tools = models.TextField(blank=False, null=False, verbose_name= 'Service / Tools Development:')
+    grant = models.TextField(blank=False, null=False, verbose_name= 'Grant / Calls Preparation:')
+    training= models.TextField(blank=False, null=False, verbose_name= 'Training / Webnars Attendance:')
+
+    def __str__(self):
+        return self.name
